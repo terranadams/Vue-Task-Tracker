7d35e5a0e6848059f73420794cc154f376a66ede
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-08-18 14:17:53 -0600

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :style="{ backgroundColor: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonVue",
+  //   props: ['text', 'color']
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <ButtonVue text="Add Task" color="green"/>
+    <ButtonVue text="Update Task" color="blue"/>
+    <ButtonVue text="Delete Task" color="red"/>
+  </header>
+</template>
+
+<script>
+import ButtonVue from './Button.vue';
+
+export default {
+  name: "HeaderVue",
+  // props: ['title']
+  props: {
+    title: String
+  },
+  components: {
+    ButtonVue
+  }
+}
+</script>
+
+
+<style scope> 
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
+    <!-- when we add 'scope' to the style tag, it limites the styling to just the elements in this component -->
